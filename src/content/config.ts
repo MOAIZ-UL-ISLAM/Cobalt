f3aeb42dde2152bcb8d220c49f8cb349105042d7
moaiz <moaiz0404@gmail.com>
2024-04-27 16:04:48 +0500

    added cards

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { z, defineCollection } from "astro:content";
+
+const ItemsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    price: z.string(),
+    sizes: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+
+export const collections = {
+  items: ItemsCollection,
+};
